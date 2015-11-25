9e8c4ac9b969dd3f96dcc713c87353e9a1336939
gorkat <thomas.gorka@iut2.upmf-grenoble.fr>
2015-11-25 11:48:59 +0100

    fichier à run tout seul si vous voulez générer un mot de passe hashé à inserer dans la bd

---
+++ Speedbooking/Controler/cryptTest.php
@@ -0,0 +1,55 @@
+<?php
+
+if(isset($_GET['mp'])) {
+    require_once '../Model/DAO.class.php';
+
+    $dao = new DAO();
+    $password = "";
+    $hash = $dao->hacherPassword($password);
+
+    //$hash = $dao->crypterPassword($password);
+    $fonctionne = password_verify($password, $hash);
+
+    if($fonctionne) {
+        $data['hash'] = $hash;
+    } else {
+        echo "erreur de hashage";
+    }
+
+}
+
+// $2y$11$Sc/DVjzHHDbZHDj2OEkj/uUSoaOaTU4eVZUdeXz9751x1TXl.Ud2y
+?>
+
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        <style>
+            .hash {
+                width : 700px;
+            }
+        </style>
+    </head>
+    
+    
+    <body>
+        <form>
+            <label> Entrer le mot de passe à Hascher :</label>
+            <input type="text" name="mp" />
+            <input type="submit" />
+        </form>
+        
+        <?php if(isset($data['hash'])){
+          echo "<label> Votre mot de passe hasché :</label>";
+          echo '<input class="hash" type="text" value="'.$data['hash'].'">';
+        } else {
+          echo "<label> Votre mot de passe hasché :</label>";
+          echo '<input class="hash" type="text" placeholder="Mot de passe hasché">';
+        }
+        ?>
+    </body>
+
+
+</html>
