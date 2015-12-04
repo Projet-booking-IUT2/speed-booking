dcc4e42a814923fa5e418fc86506834581c3bee3
grondinje <jeremy.grondin@iut2.upmf-grenoble.fr>
2015-12-04 16:05:22 +0100

    Creation vue pour nouveau groupe, modification vue interne groupe.

--- Speedbooking/Views/view.interne.groupe.php
+++ Speedbooking/Views/view.interne.groupe.php
@@ -6,8 +6,9 @@
 
         <li><a href="../Controler/date.ctrl.php"><span class="glyphicon glyphicon-calendar"></span> Mes dates</a></li>
         <li><a href="../Controler/contacts.ctrl.php"><span class="glyphicon glyphicon-phone-alt"></span> Mes contacts</a></li>
+        <li class="active"><a href="../Controler/groupe.ctrl.php"><span class="glyphicon glyphicon-phone-alt"></span> Mes groupes</a></li>
         <li><a href="../Controler/fichiers.ctrl.php" ><span class="glyphicon glyphicon-file"></span> Mes fichiers</a></li>
-        <li class="active"><a href="#" ><span class="glyphicon glyphicon-user"></span> Mon compte</a></li>
+        <li><a href="#" ><span class="glyphicon glyphicon-user"></span> Mon compte</a></li>
         <li><a href="../Controler/portail.ctrl.php" ><span class="glyphicon glyphicon-log-out"></span> Déconnexion</a></li>
       </ul>
     </div>
@@ -27,11 +28,9 @@
     </div>
     <div class="panel-body">
             <form action="../controler/groupe.ctrl.php" method="post" class= " well form ">
-                <div class="row">
-                      <fieldset class="form-group">
-                       <label for="nomG" class="col-md-4 control-label">Nom du groupe : </label>
-                       <div class="col-md-8"><input type="text" id="nom" class="form-control" name="c_nom" value ="<?= $data['groupe']['nom'] ?>" /></div>
-                      </fieldset>
+                <div class="from-group row">
+                    <label for="nomG" class="col-md-4 control-label">Nom du groupe : </label>
+                    <div class="col-md-8"><input type="text" id="nom" class="form-control" name="c_nom" value ="<?= $data['groupe']['nom'] ?>" /></div>
                 </div>
                 <div class=" from-group row">
                     <label for="nom" class="col-md-4 control-label">Membres du groupe : </label>
@@ -39,16 +38,17 @@
                         //récup les membres du groupes et renvoie sur leur fiche contact si clique sur leur nom
                     ?>
                 </div>
+                <div class="form-group row">
+                    <label for="mail"  class="col-md-2 control-label">Mail : </label>
+                    <div class="col-md-10"><input type="email" id="mail" class="form-control" name="c_mail"/></div>
+                </div>
+                <div class="form-group row">
+                    <label for="style"  class="col-md-2 control-label">Style : </label>
+                    <div class="col-md-10"><input type="text" id="mail" class="form-control" name="c_style" value ="<?= $data['groupe']['style'] ?>" /></div>
+                </div>
                 <div class="from-group row">
                      <label for="textarea" class="col-md-2 control-label">Notes :</label>
-                    <div class="col-md-10"><textarea id="textarea" class="form-control" rows="2" name="c_notes"><?= $data['groupe']['notes'] ?></textarea></div>
-                </div>
-                <div class="form-group">
-                <label  class="col-md-4 control-label">Date de mise à jour minimum :</label>
-                    <input type="radio" name ="c_dureeMAJ" value="1_month">1 mois
-                    <input type="radio" name ="c_dureeMAJ" value="3_month" checked>3 mois
-                    <input type="radio" name ="c_dureeMAJ" value="6_month">6 mois
-                    <input type="radio" name ="c_dureeMAJ" value="12_month">12 mois
+                    <div class="col-md-10"><textarea id="textarea" class="form-control" rows="2" name="c_notes"></textarea></div>
                 </div>
                 <div class="form-group row">
                     <button type="submit" class="btn btn-success pull-right boutonPlus"><span class="glyphicon glyphicon-ok">Créer</span></button>
@@ -60,5 +60,8 @@
 </div>
 </section>
 <?php
-    }?>
+    } else{
+        include '../Views/view.interne.groupeNouveau.php';
+    }
+?>
 <?php include('../Views/view.interne.footer.php'); ?>
