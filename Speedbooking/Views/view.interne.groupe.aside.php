7ecfdbcc0299265a2d2cf28ea99271ba29a83367
grondinje <jeremy.grondin@iut2.upmf-grenoble.fr>
2015-12-04 13:36:12 +0100

    Début de php pour récuperer les groupes.

--- Speedbooking/Views/view.interne.groupe.aside.php
+++ Speedbooking/Views/view.interne.groupe.aside.php
@@ -1,21 +1,33 @@
 <!-- Vue de création de groupe
-A faire : code php pour la création
+
 
 -->
 
 <div class="col-md-4">
   <div class="panel panel-info">
     <div class="panel-heading">
-      <h3 class="panel-title">Vos Groupes :</h3>
+        <div class="row">
+            <h3 class="panel-title">Vos Groupes :</h3>
+            <a href="../Controler/groupe.ctrl.php" class="btn btn-lg btn-success pull-right boutonPlus"><span class="glyphicon glyphicon-plus"></span></a>
+        </div>
     </div>
     <div class="panel-body">
-        <ul class="list-group">
+        <ul class="list-group list-unstyled">
         <?php
-            echo "
-            <div class=\"alert alert-info\">
-            <strong>Vous n'avez pas encore enregistré de groupe ! </strong>
-            </div>
-            ";
+            if(isset($data['AllGroupe'])){
+                echo "
+                <div class=\"alert alert-info\">
+                <strong>Vous n'avez pas encore enregistré de groupe ! </strong>
+                </div>
+                ";
+            }
+            else {
+                foreach($data['AllGroupe'] as $c ) {
+                    $nom=$c['nom'];
+                    echo '<li><a href="../Controler/groupe.ctrl.php?selected='.$nom.'" class="list-group-item">'.
+              "<span class=\"glyphicon glyphicon-chevron-right pull-right\"></span> $nom</a></li>";
+                }
+            }
          ?>
         </ul>
     </div>
--- Speedbooking/Views/view.interne.groupe.php
+++ Speedbooking/Views/view.interne.groupe.php
@@ -6,9 +6,8 @@
 
         <li><a href="../Views/view.interne.date.php"><span class="glyphicon glyphicon-calendar"></span> Mes dates</a></li>
         <li><a href="../Views/view.interne.contacts.php"><span class="glyphicon glyphicon-phone-alt"></span> Mes contacts</a></li>
-        <li class="active"><a href="#"><span class="glyphicon glyphicon-user"></span> Mes groupes</a></li>
         <li><a href="#" ><span class="glyphicon glyphicon-file"></span> Mes fichiers</a></li>
-        <li><a href="#" ><span class="glyphicon glyphicon-user"></span> Mon compte</a></li>
+        <li class="active"><a href="#" ><span class="glyphicon glyphicon-user"></span> Mon compte</a></li>
         <li><a href="#" ><span class="glyphicon glyphicon-log-out"></span> Déconnexion</a></li>
       </ul>
     </div>
@@ -20,51 +19,39 @@
     <div class="panel-heading">
       <h3 class="panel-title">
         <?php
-            echo "Nouveau Groupe :";
-            //echo $data['groupe']['nom'];
+            echo $data['groupe']['nom'];
         ?></h3>
     </div>
     <div class="panel-body">
-      <!--<form action="../controler/ctrl.interne.groupe.php" method="post" class= " well form ">-->
-      <?php
-          //formulaire rempli
-          ?>
-          <div class="row">
-                <fieldset class="form-group">
-                 <label for="nomG" class="col-md-4 control-label">Nom du groupe : </label>
-                 <div class="col-md-8"><input type="text" id="nomG" class="form-control" name="c_nomG" ></div>
-                </fieldset>
-          </div>
-          <div class="row">
-              <fieldset class="form-group">
-                 <label for="nom" class="col-md-4 control-label">Membres du groupe : </label>
-                  <?php
+            <form action="../controler/ctrl.interne.groupe.php" method="post" class= " well form ">
+                <div class="row">
+                      <fieldset class="form-group">
+                       <label for="nomG" class="col-md-4 control-label">Nom du groupe : </label>
+                       <div class="col-md-8"><input type="text" id="nom" class="form-control" name="c_nom" value ="<?= $data['groupe']['nom'] ?>" /></div>
+                      </fieldset>
+                </div>
+                <div class=" from-group row">
+                    <label for="nom" class="col-md-4 control-label">Membres du groupe : </label>
+                    <?php
                         //récup les membres du groupes et renvoie sur leur fiche contact si clique sur leur nom
-                  ?>
-              </fieldset>
-          </div>
-        <?php 
-          //formulaire vide
-        ?>
-        <div class="row">
-            <fieldset>
-                  <label for="textarea" class="col-md-4 control-label">Note :</label>
-                  <textarea id="textarea" class="form-control" rows="2" name="c_notes"></textarea>
-                  <div class="form-group">
-                      <label  class="col-md-4 control-label">Date de mise à jour minimum :</label>
-                      <input type="radio" name ="c_dureeMAJ" value="1_month">1 mois
-                      <input type="radio" name ="c_dureeMAJ" value="3_month" checked>3 mois
-                      <input type="radio" name ="c_dureeMAJ" value="6_month">6 mois
-                      <input type="radio" name ="c_dureeMAJ" value="12_month">12 mois
-                  </div>
-            </fieldset>
-          </div>
-        <?php 
-        ?>
-        <div class="form-group row">
-            <button type="submit" class="btn btn-success pull-right boutonPlus"><span class="glyphicon glyphicon-ok">Créer</span></button>
+                    ?>
+                </div>
+                <div class="from-group row">
+                     <label for="textarea" class="col-md-2 control-label">Notes :</label>
+                    <div class="col-md-10"><textarea id="textarea" class="form-control" rows="2" name="c_notes"><?= $data['groupe']['notes'] ?></textarea></div>
+                </div>
+                <div class="form-group">
+                <label  class="col-md-4 control-label">Date de mise à jour minimum :</label>
+                    <input type="radio" name ="c_dureeMAJ" value="1_month">1 mois
+                    <input type="radio" name ="c_dureeMAJ" value="3_month" checked>3 mois
+                    <input type="radio" name ="c_dureeMAJ" value="6_month">6 mois
+                    <input type="radio" name ="c_dureeMAJ" value="12_month">12 mois
+                </div>
+                <div class="form-group row">
+                    <button type="submit" class="btn btn-success pull-right boutonPlus"><span class="glyphicon glyphicon-ok">Créer</span></button>
+                </div>
+            </form>
         </div>
-      </form>
     </div>
   </div>
 </div>
